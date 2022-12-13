01eee595eecb50821f2d728854ac09a09dc01f39
kun yang <yangkun529753183@gmail.com>
2022-12-13 19:49:45 +0100

    state updated

--- bachelor Project/frontend/src/App.js
+++ bachelor Project/frontend/src/App.js
@@ -3,25 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register"; 
+import { Logged } from "./Logged";
 
 
 function App() {
   //那个页面默认显示 所以默认为login页面
   const [currentForm, setCurrentForm] = useState('login'); 
-
+  const [currentState, setCurrentState] = useState('unlogged');
   //切换表单 toggle form
   const toggleForm = (formName) => {
     //这是一个setCurrentForm Hook抓取表单的名称 现在我们需要使用这个函数并将其传递给我们当前的表单
     setCurrentForm(formName);
   }
 
-  return (
-    <div className="App">
-      {
-        currentForm === 'login' ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-    </div>
-  );
+  const toggleState = (State) => {
+    //这是一个setCurrentForm Hook抓取表单的名称 现在我们需要使用这个函数并将其传递给我们当前的表单
+    setCurrentState(State);
+  }
+    return (
+      <div className="App">
+        {currentState === 'unlogged' ? <Login onStateSwitch={toggleState} /> : <Register onStateSwitch={toggleState} />}
+        {currentForm === 'login' ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
+        
+        
+      </div>
+    );
 }
 
 export default App;
---
+++ bachelor Project/frontend/src/Logged.jsx
@@ -0,0 +1,5 @@
+export const Logged =() => {
+    return(
+        <> Logged </>
+    )
+}
