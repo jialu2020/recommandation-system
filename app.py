451568eabb97812dd35f5260dcd423c8e6c43fd8
lujia4723 <lujia4723@gmail.com>
2023-01-02 01:03:19 +0100

    POST GET PUT methodes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 import psycopg2
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build/static')
 
 api = Api(app)
 
@@ -14,23 +16,21 @@ CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:472372239@localhost/users"
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 flask_migrate = Migrate(app, db)
 
 
 # create a class UserModel
 class Users(db.Model):
-
     __tablename__ = "studentUsers"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String())
+    username = db.Column(db.String(), unique=True, )
     password = db.Column(db.String())
 
-
-    def init(self, username, password): #constractor for the object
+    def __init__(self, username, password):  # constractor for the object
         self.username = username
         self.password = password
-
 
 
 class Exercises(db.Model):
@@ -39,30 +39,63 @@ class Exercises(db.Model):
     aufgabenstellung = db.Column(db.String())
     musterloesung = db.Column(db.String())
 
-    def init(self, aufgabenstellung, musterloesung):  # constractor for the object
+    def __init__(self, aufgabenstellung, musterloesung):  # constractor for the object
         self.aufgabenstellung = aufgabenstellung
         self.musterloesung = musterloesung
 
+    def __repr__(self):
+        return '<username{}'.format(self.username)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "password")
+
+
+user_schema = UserSchema()
+user_schema = UserSchema(many=True)
 
 
 
 
+@app.route("/get", methods=["GET"])
+def get_users():
+    all_users = Users.query.all()
+    results = user_schema.dump(all_users)
+    return jsonify(results)
 
 
-
-@app.route("/", methods= ["GET", "POST"])
-
-def submit():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password "]
-
-        student= Users(username,password)
-        db.session.add(student)
-        db.session.commit()
+@app.route("/get/<id>", methods=["GET"])
+def get_user_with_id(id):
+    user = Users.query.get(id)
+    return UserSchema().jsonify(user)
 
 
+@app.route("/add", methods=['POST'])
+def add_users():
+    username = request.json["username"]
+    password = request.json["password"]
+
+    users = Users(username, password)
+    db.session.add(users)
+    db.session.commit()
+    return UserSchema().jsonify(users)
 
 
+@app.route("/update/<id>", methods=["PUT"])
+def update_user_with_id(id):
+    user = Users.query.get(id)
+
+    username = request.json["username"]
+    password = request.json["password"]
+
+    user.username = username
+    user.password = password
+
+    db.session.commit()
+
+    return UserSchema().jsonify(user)
 
 
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=5000, debug=True)
