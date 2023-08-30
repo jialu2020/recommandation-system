bfad0d405968485d59f2308c8993d2872b4a4ea5
lujia <lujia4723@gmail.com>
2023-08-30 13:18:02 +0200

    refactor the UI from TerminationPage.js

--- bachelor Project/frontend/src/pages/TerminationPage.js
+++ bachelor Project/frontend/src/pages/TerminationPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './TerminationPage.css';
+import sadIcon from '../icons/sad.png';
 
 const TerminationPage = () => {
   return (
-    <div>
-      <h1>Studie Beenden</h1>
-      <p>Ich bedauere, dass Sie die Studie beenden möchten.</p>
-      <p>Bitte kontaktieren Sie uns per E-Mail, um Ihre gesammelten Informationen zu löschen:</p>
-      <p>Kontakt-E-Mail: jia.lu.2@stud.uni-due.de</p>
-      <p>Bitte geben Sie in Ihrer E-Mail an, warum Sie die Studie beenden möchten und teilen Sie uns Ihren Benutzernamen mit.</p>
+    <div className="page-container">
+      <div className="page-content">
+        <img src={sadIcon} alt="Sad Icon" className="sad-icon" />
+        <h1 className="page-title">Studie Beenden</h1>
+        <p className="page-text">Ich bedauere, dass Sie die Studie beenden möchten.</p>
+        <p className="page-text">Bitte kontaktieren Sie uns per E-Mail, um Ihre gesammelten Informationen zu löschen:</p>
+        <p className="contact-email">
+          Kontakt-E-Mail: <a href="mailto:jia.lu.2@stud.uni-due.de">jia.lu.2@stud.uni-due.de</a>
+        </p>
+        <p className="email-instructions">Bitte geben Sie in Ihrer E-Mail an, warum Sie die Studie beenden möchten und teilen Sie uns Ihren Benutzernamen mit.</p>
+      </div>
     </div>
   );
 };
